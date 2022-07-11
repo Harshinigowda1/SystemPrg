c254c625ddb9af991d451733972b44bb23d7d91a
Harshinigowda1 <108176912+Harshinigowda1@users.noreply.github.com>
2022-07-11 14:34:45 +0530

    Add files via upload

---
+++ concurrent.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+using namespace std;
+int main()
+{
+    int pid;
+    int i = 1;
+
+    for(;;)
+    {
+        pid = fork();
+        if(pid < 0){
+            cout<<"Maximum no of concurrent process are"<<i;
+            exit(0);
+        }
+        if(pid == 0)
+            i++;
+        else{
+            wait(0);
+            exit(0);
+        }
+    }
+
+    return 0;
+}
