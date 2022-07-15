c95110e6f3fdabd8d1ab1915823a867d9a65696a
Harshinigowda1 <108176912+Harshinigowda1@users.noreply.github.com>
2022-07-15 10:04:59 +0530

    Add files via upload

---
+++ CW/CW assign-12.07/inc/Employee.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<iostream>
+
+class Employee {
+  int ID;
+  string name;
+  string role;
+  long int salary;
+
+public:
+    Employee() {}
+    void initEmployee(int id, string varName, string varRole, long sal) {
+        ID = id,
+        name = varName,
+        role = varRole,
+        salary = sal;
+    }
+};
